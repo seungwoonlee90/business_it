f18e788e12026c23b07cc2de2991cc7d27b04daf
ethanlee <seungwoonlee90@gmail.com>
2021-09-09 16:56:27 +0900

    create Navigation

---
+++ HCI/teamproject/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { HashRouter } from 'react-router-dom';
+import Navigation from './components/Navigation';
+
+
+function App() {
+  return <HashRouter>
+    <Navigation />
+  </HashRouter>
+}
+
+export default App;
