2e43a9a367adb8f63e7c5b527f2ef1ba65ec7d53
ethanlee <seungwoonlee90@gmail.com>
2021-09-27 16:18:03 +0900

    Update : Main.js 내용 수정

--- HCI/teamproject_pwa/src/components/Main.js
+++ HCI/teamproject_pwa/src/components/Main.js
@@ -1,18 +1,18 @@
 import React from 'react';
-import { Container, Row, Col, Image, Button } from 'react-bootstrap';
+import { Container, Row, Col, Image } from 'react-bootstrap';
 
 function Main() {
     return (
         <div className="mainProduct">
             <Container>
                 <Row style={{justifyContent: "center"}}>
-                    <Col xs={4} >
+                    <Col xs={'4'} >
                         <Image src="https://firebasestorage.googleapis.com/v0/b/teamproject-4b829.appspot.com/o/%E1%84%88%E1%85%A2%E1%84%85%E1%85%A91.png?alt=media&token=419a94da-fd49-46d0-941d-ae912b0da91c" rounded width="100%" />
                     </Col>
-                    <Col xs={6} style={{backgroundColor: "dimgrey"}} >
-                        <h1>Title</h1>
-                        <h4>Descriptions</h4>
-                        <Button variant="light" style={{marginBottom: "3%"}}  >Learn More</Button>
+                    <Col xs={'6'} style={{paddingTop:"1%"}}  >
+                        <h4> 🐈‍⬛</h4>
+                        <p style={{color: "grey"}}>2021년 9월 27일</p>
+                        <p>TV가 재미없는 빼로</p>
                     </Col>
                 </Row>
             </Container>
