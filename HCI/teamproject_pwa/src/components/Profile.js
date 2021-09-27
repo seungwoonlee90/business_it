397c9ec76b2586ad5c93412a3c60a30870062212
ethanlee <seungwoonlee90@gmail.com>
2021-09-27 21:02:49 +0900

    Update : Profile.js baseline code

--- HCI/teamproject_pwa/src/components/Profile.js
+++ HCI/teamproject_pwa/src/components/Profile.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import './Profile.css';
 
 function Profile(){
     return (
-        <div>
-            <h4>Profile Page</h4>
+        <div className="profileBody">
+            <article className="profile">
+                <img src="https://doodleipsum.com/600?shape=circle&bg=ceebff" alt="profile"/>
+                <h1>teamproject_admin</h1>
+                <h2>@HCI</h2>
+            </article>
+            <ul className="contact">
+                <li>
+                    <span>admin@gmail.com</span>
+                </li>		
+            </ul>
+            <button>Log out</button>
         </div>
     )
 }
