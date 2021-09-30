6226b10905d2ff1d287a9575d1778985315497dd
ethanlee <seungwoonlee90@gmail.com>
2021-09-30 15:35:46 +0900

    Update : Navigation.js

--- HCI/teamproject_pwa/src/components/Navigation.js
+++ HCI/teamproject_pwa/src/components/Navigation.js
@@ -4,14 +4,16 @@ import { Navbar, Container, Nav } from 'react-bootstrap';
 
 function Navigation(){
     return(
-        <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+        <Navbar bg="dark" variant="dark">
             <Container>
-            <Navbar.Brand as={Link} to="/">OH!Dang Dang</Navbar.Brand>
-            <Nav className="me-auto">
-            <Nav.Link as={Link} to="/profile">Profile</Nav.Link>
-            </Nav>
+                <Navbar.Brand as={Link} to="/">OH!Dang Dang</Navbar.Brand>
+                <Nav className="me-auto">
+                    <Nav.Link as={Link} to="/profile">Profile</Nav.Link>
+                    <Nav.Link as={Link} to="/about">About</Nav.Link>
+                </Nav>
             </Container>
         </Navbar>
+        
     )
 }
 
