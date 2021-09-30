6aec4a3a1797f5a008a538dc09674705d40dd098
ethanlee <seungwoonlee90@gmail.com>
2021-09-30 16:14:37 +0900

    Update : About.js copyrights

--- HCI/teamproject_pwa/src/components/About.js
+++ HCI/teamproject_pwa/src/components/About.js
@@ -3,7 +3,7 @@ import React from 'react';
 function About() {
     return (
         <div>
-            
+            <footer>&copy; {new Date().getFullYear()} HCI TEAM-2 All Rights Reserved.</footer>
         </div>
     )
 }
