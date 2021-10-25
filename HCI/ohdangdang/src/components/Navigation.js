2f388481fee340984262a6cec4edbd64c243a0c2
ethanlee <seungwoonlee90@gmail.com>
2021-10-25 12:41:11 +0900

    Update : Color change

--- HCI/ohdangdang/src/components/Navigation.js
+++ HCI/ohdangdang/src/components/Navigation.js
@@ -11,13 +11,13 @@ function Navigation() {
             alt="cover"
           />
         </Link>
-        <Link to="/signin" className="issue mouse-effect">
+        <Link to="/signin" className="issue mouse-effect active">
           <img
             src="https://raw.githubusercontent.com/anjihhg/billy_htmlcss/2c3e87f182df920a4eb166e0dbda6658dc6fa0eb/img/icon/issue.svg"
             alt="issue"
           />
         </Link>
-        <Link to="/about" className="like mouse-effect">
+        <Link to="/about" className="like mouse-effect active">
           <img
             src="https://raw.githubusercontent.com/anjihhg/billy_htmlcss/2c3e87f182df920a4eb166e0dbda6658dc6fa0eb/img/icon/cover.svg"
             alt="issue"
