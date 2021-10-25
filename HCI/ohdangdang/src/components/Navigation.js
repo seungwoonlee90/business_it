665c765151feb0c358c57a3c086cc5af65d5eb02
ethanlee <seungwoonlee90@gmail.com>
2021-10-25 12:57:59 +0900

    Update : icon

--- HCI/ohdangdang/src/components/Navigation.js
+++ HCI/ohdangdang/src/components/Navigation.js
@@ -6,22 +6,13 @@ function Navigation() {
     <>
       <nav className="gnb">
         <Link to="/" className="cover mouse-effect active">
-          <img
-            src="https://raw.githubusercontent.com/anjihhg/billy_htmlcss/2c3e87f182df920a4eb166e0dbda6658dc6fa0eb/img/icon/like.svg"
-            alt="cover"
-          />
+          <span class="material-icons">favorite</span>
         </Link>
         <Link to="/signin" className="issue mouse-effect active">
-          <img
-            src="https://raw.githubusercontent.com/anjihhg/billy_htmlcss/2c3e87f182df920a4eb166e0dbda6658dc6fa0eb/img/icon/issue.svg"
-            alt="issue"
-          />
+          <span class="material-icons">account_circle</span>
         </Link>
         <Link to="/about" className="like mouse-effect active">
-          <img
-            src="https://raw.githubusercontent.com/anjihhg/billy_htmlcss/2c3e87f182df920a4eb166e0dbda6658dc6fa0eb/img/icon/cover.svg"
-            alt="issue"
-          />
+          <span class="material-icons">help</span>
         </Link>
       </nav>
     </>
