2c23368ac5900b557dacb0354eadbeae7fbd845c
ethanlee <seungwoonlee90@gmail.com>
2021-10-28 12:35:19 +0900

    Update : main youtube

--- HCI/ohdangdang/src/components/Main.js
+++ HCI/ohdangdang/src/components/Main.js
@@ -1,6 +1,12 @@
 import React from "react";
+import YouTube from "react-youtube";
 
 function Main() {
+  const opts = {
+    playerVars: {
+      autoplay: 0,
+    },
+  };
   return (
     <>
       <div className="text-area">
@@ -12,12 +18,18 @@ function Main() {
       </div>
 
       <ul className="list">
-        <li className="item"></li>
-        <li className="item"></li>
-        <li className="item"></li>
-        <li className="item"></li>
-        <li className="item"></li>
-        <li className="item"></li>
+        <li className="item">
+          <YouTube className="youtube" videoId="kqEpePJmv4I" opts={opts} />;
+        </li>
+        <li className="item">
+          <YouTube className="youtube" videoId="wGMwDY8lCaM" opts={opts} />;
+        </li>
+        <li className="item">
+          <YouTube className="youtube" videoId="E541z2r1Oyg" opts={opts} />;
+        </li>
+        <li className="item">
+          <YouTube className="youtube" videoId="2g811Eo7K8U" opts={opts} />;
+        </li>
       </ul>
     </>
   );
--- HCI/ohdangdang/src/components/Upload.js
+++ HCI/ohdangdang/src/components/Upload.js
@@ -1,12 +1,10 @@
 import React from "react";
-import Youtube from "./Youtue";
 
 function Upload() {
   return (
     <div className="developing">
       <h4>developing...</h4>
       <span className="material-icons">loop</span>
-      <Youtube />
     </div>
   );
 }
