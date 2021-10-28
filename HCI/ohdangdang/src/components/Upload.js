b660d514bb4d3f6384b81ee4ce8d9bded7d82fc6
ethanlee <seungwoonlee90@gmail.com>
2021-10-28 11:48:49 +0900

    youtube test

--- HCI/ohdangdang/src/components/Upload.js
+++ HCI/ohdangdang/src/components/Upload.js
@@ -1,10 +1,12 @@
 import React from "react";
+import Youtube from "./Youtue";
 
 function Upload() {
   return (
     <div className="developing">
       <h4>developing...</h4>
       <span className="material-icons">loop</span>
+      <Youtube />
     </div>
   );
 }
