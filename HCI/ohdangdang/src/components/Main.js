df3dc3c22a1dd0aac32888d16b3b9cb541198f9f
ethanlee <seungwoonlee90@gmail.com>
2021-11-02 22:21:28 +0900

    Update : main.js

--- HCI/ohdangdang/src/components/Main.js
+++ HCI/ohdangdang/src/components/Main.js
@@ -6,32 +6,35 @@ function Main() {
     playerVars: {
       autoplay: 0,
       playsinline: 1,
+      origin: "https://seungwoonlee90.github.io/business_it/",
     },
   };
   return (
     <>
-      <div className="text-area">
-        <h1>OH! DangDang</h1>
-        <div className="desc">
-          We'll make a mobile app <br />
-          using react and firebase.
+      <div className="mainForm">
+        <div className="text-area">
+          <h1>OH! DangDang</h1>
+          <div className="desc">
+            We'll make a mobile app <br />
+            using react and firebase.
+          </div>
         </div>
-      </div>
 
-      <ul className="list">
-        <li className="item">
-          <YouTube className="youtube" videoId="kqEpePJmv4I" opts={opts} />;
-        </li>
-        <li className="item">
-          <YouTube className="youtube" videoId="wGMwDY8lCaM" opts={opts} />;
-        </li>
-        <li className="item">
-          <YouTube className="youtube" videoId="E541z2r1Oyg" opts={opts} />;
-        </li>
-        <li className="item">
-          <YouTube className="youtube" videoId="2g811Eo7K8U" opts={opts} />;
-        </li>
-      </ul>
+        <ul className="list">
+          <li className="item">
+            <YouTube className="youtube" videoId="kqEpePJmv4I" opts={opts} />;
+          </li>
+          <li className="item">
+            <YouTube className="youtube" videoId="wGMwDY8lCaM" opts={opts} />;
+          </li>
+          <li className="item">
+            <YouTube className="youtube" videoId="E541z2r1Oyg" opts={opts} />;
+          </li>
+          <li className="item">
+            <YouTube className="youtube" videoId="2g811Eo7K8U" opts={opts} />;
+          </li>
+        </ul>
+      </div>
     </>
   );
 }
