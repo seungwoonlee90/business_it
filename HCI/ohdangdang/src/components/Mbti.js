1420c65d5b1f5b9332a0a37845b124456bc7988e
ethanlee <seungwoonlee90@gmail.com>
2021-11-26 21:52:34 +0900

    Update Mbti questions

--- HCI/ohdangdang/src/components/Mbti.js
+++ HCI/ohdangdang/src/components/Mbti.js
@@ -5,7 +5,7 @@ function Mbti() {
   return (
     <div className="mbti">
       <img src={main} alt="main" width="82%"/>
-      <div>123,456명이 인정한<br />우리 댕댕이를 위한<br />MBTI TEST</div>
+      <div><p style={{"textDecoration" : "underline", "display":"inline", "color":"black", "fontSize":"35px"}}>123,456</p>&nbsp; 명이 인정한<br />우리 댕댕이를 위한<br />MBTI TEST</div>
       <div><button onClick={(e)=>{window.location.hash = "/mbti_info";}}>Get Started</button></div>
     </div>
   )
--- HCI/ohdangdang/src/components/Mbti_questions.js
+++ HCI/ohdangdang/src/components/Mbti_questions.js
@@ -38,9 +38,10 @@ function Question() {
     <section>
       <div className="mbti">
         <div className="back"><span className="material-icons" onClick={()=>{window.location.hash = "/mbti_info";}}>arrow_back</span></div>
-        <img src={main} alt="main" width="35%"/>
+        <img src={main} alt="main" width="40%"/>
         <div>Question 01</div>
-        <div className="question">산책을 하다 처음 보는 <br /> 사람(동물)을 발견했다. <br />우리 반려동물은</div>
+        <div style={{"fontSize" : "15px"}}>(1/5)</div>
+        <div className="question">산책을 하다 처음 보는 사람(동물)을 <br /> 발견했다. 우리 반려동물은 ?</div>
         <div>
           <button onClick={next}>도망가거나 뒤로 숨는다</button>
           <button onClick={next}>꼬리를 흔들며 다가간다</button>
@@ -51,9 +52,10 @@ function Question() {
     <section>
       <div className="mbti">
         <div className="back"><span className="material-icons" onClick={back}>arrow_back</span></div>
-        <img src={fire} alt="main" width="35%"/>
+        <img src={fire} alt="main" width="40%"/>
         <div>Question 02</div>
-        <div className="question">주인이 없을 때, 우리 반려동물은</div>
+        <div style={{"fontSize" : "15px"}}>(2/5)</div>
+        <div className="question">주인이 없을 때, <br /> 우리 반려동물은 ?</div>
         <div>
           <button onClick={next}>현관문을 바라보며 주인을 기다린다</button>
           <button onClick={next}>혼자 잘 놀거나 티비를 본다</button>
@@ -64,9 +66,10 @@ function Question() {
     <section>
       <div className="mbti">
         <div className="back"><span className="material-icons" onClick={back}>arrow_back</span></div>
-        <img src={twe} alt="main" width="35%"/>
+        <img src={twe} alt="main" width="40%"/>
         <div>Question 03</div>
-        <div className="question">식사시간! 사료를 주면, 우리 반려동물은</div>
+        <div style={{"fontSize" : "15px"}}>(3/5)</div>
+        <div className="question">식사시간! 사료를 주면,<br /> 우리 반려동물은 ?</div>
         <div>
           <button onClick={next}>주위를 경계하며 먹는다</button>
           <button onClick={next}>주인이 만져도 모를정도로 집중해 먹는다</button>
@@ -77,9 +80,10 @@ function Question() {
     <section>
       <div className="mbti">
         <div className="back"><span className="material-icons" onClick={back}>arrow_back</span></div>
-        <img src={sad} alt="main" width="35%"/>
+        <img src={sad} alt="main" width="40%"/>
         <div>Question 04</div>
-        <div className="question">내가 울면, 우리 반려동물은</div>
+        <div style={{"fontSize" : "15px"}}>(4/5)</div>
+        <div className="question">내가 울면,<br/> 우리 반려동물은 ?</div>
         <div>
           <button onClick={next}>가까이 다가와 옆에 앉아있는다</button>
           <button onClick={next}>꼬리를 흔들며 재롱을 부린다</button>
@@ -90,9 +94,10 @@ function Question() {
     <section>
       <div className="mbti">
         <div className="back"><span className="material-icons" onClick={back}>arrow_back</span></div>
-        <img src={bing} alt="main" width="35%"/>
+        <img src={bing} alt="main" width="40%"/>
         <div>Question 05</div>
-        <div className="question">우리 반려동물은 자율배식을 하고 있다.</div>
+        <div style={{"fontSize" : "15px"}}>(5/5)</div>
+        <div className="question">우리 반려동물은,<br /> 자율배식을 하고 있다 ?</div>
         <div>
           <button onClick={next}>그렇다</button>
           <button onClick={next}>아니다</button>
